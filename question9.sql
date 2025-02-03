636702d76288a677a4326ddb1aa3c56e320caa39
khushboodixit03 <165786244+khushboodixit03@users.noreply.github.com>
2025-02-03 23:47:20 +0530

    Update question9.sql

--- question9.sql
+++ question9.sql
@@ -1,1 +1,11 @@
 
+CREATE TABLE Employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    dept_id INT
+);
+
+CREATE TABLE Departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
